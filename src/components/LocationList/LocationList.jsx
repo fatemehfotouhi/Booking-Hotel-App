653b592e10e8bff9225f7eee36f39a3c38e2c138
Fatemeh Fotouhi <fatemeh.fotouhi4@gmail.com>
2023-12-11 14:25:28 +0330

    edited URL

--- src/components/LocationList/LocationList.jsx
+++ src/components/LocationList/LocationList.jsx
@@ -2,7 +2,7 @@ import useFetch from "../../hooks/useFetch";
 import Loader from "../Loader/Loader";
 
 function LocationList() {
-    const { data, isLoading } = useFetch("https://booking-hotel-app-4vvx.vercel.app/hotels", "");
+    const { data, isLoading } = useFetch("http://localhost:5000/hotels", "");
     if (isLoading) return <Loader />
     return (
         <div className="locationsContainer">
